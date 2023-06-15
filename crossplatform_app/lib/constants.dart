a6b676fa78b2b22c9fdc7652ea6db0e954240ba5
Sujata443 <69589592+Sujata443@users.noreply.github.com>
2023-06-15 23:02:37 +0530

    Signin and login page UI

---
+++ crossplatform_app/lib/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color.fromARGB(255, 104, 146, 51);
+const kPrimaryLightColor = Color.fromARGB(255, 217, 235, 193);
+const darkGreenColor = Color.fromARGB(1, 6, 149, 49);
+const white = Color.fromARGB(255, 255, 255, 255);
+const login_page_background = Color.fromARGB(1, 250, 250, 252);
+
+const double defaultPadding = 16.0;
